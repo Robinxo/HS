543b778ef3ed45addd7cac393e28c2f7a46cf61c
om rawat <omrawat.work@gmail.com>
2025-09-13 11:20:13 +0530

    Hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,10 +11,10 @@ export default function HeroSection() {
     >
       <div className="relative z-10 text-center px-4 sm:px-6 md:px-8 py-12 font-Bricolage">
         <h2 className="text-4xl md:text-6xl font-bold text-yellow-400 drop-shadow-[3px_2px_0px_rgba(0,0,0,0.8)]">
-          TANVI GROUP&apos;s
+          TANVI GROUP&apos;
         </h2>
         <h1 className="text-6xl md:text-9xl font-extrabold text-yellow-400 drop-shadow-[4px_2px_0px_rgba(0,0,0,0.8)] mb-4">
-          Hotels & Resorts
+          Hotels & Resort
         </h1>
         <span className="block text-lg md:text-xl max-w-4xs mx-auto mt-4 text-yellow-400">
           At <strong>Tanvi Premium</strong>, we offer more than just a place to stay — we create experiences defined by comfort, care, and heartfelt hospitality. Whether you're traveling for business or leisure, our commitment is to make every moment memorable. With personalized service, modern amenities, and a warm, welcoming atmosphere, we strive to feel like home — and better.

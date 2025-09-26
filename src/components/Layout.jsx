e5d53b9a220b432d8609373560a86550a9259c5b
Robinxo <omrawat.work@gmail.com>
2025-09-26 18:27:08 +0530

    Whatsapp integration

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,18 @@
 import Navbar from './Navbar.jsx';
 import Testimonials from './Reviews.jsx';
 import Footer from './footer.jsx';
+import WhatsAppBubble from './WhatsappWidget.jsx';
 
 export default function Layout({ children }) {
   return (
     <div className='min-h-screen flex flex-col bg-white'>
       <Navbar />
       <main className='flex-1'>{children}</main>
+      <WhatsAppBubble
+        phone='919876543210'
+        message='Hello, I need help with a booking.'
+        showBadge
+      />
       <Testimonials />
       <Footer />
     </div>
